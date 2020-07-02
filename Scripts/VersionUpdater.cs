d44258640b394f15d336af300fd61e8e5671c131
OwO <kculwp@gmail.com>
2020-07-02 16:58:41 +0800

    Life Improvement

--- Scripts/VersionUpdater.cs
+++ Scripts/VersionUpdater.cs
@@ -1,11 +1,12 @@
-using JetBrains.Annotations;
+#if UNITY_EDITOR
 using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 public class VersionUpdater : MonoBehaviour
@@ -20,71 +21,101 @@ public class VersionUpdater : MonoBehaviour
     }
     public VT VersionType;
 
-    string BuildPath;
-    string DataPath;
-
-#if UNITY_EDITOR
-    private void Awake()
-    {
-        BuildPath = Application.dataPath.ToString() + "/BuildInfo/BuildInfo.json";
-        DataPath = Application.dataPath.ToString().TrimEnd("Assets".ToCharArray()) + "ProjectSettings/ProjectSettings.asset";
-    }
-
     void Start()
     {
-        AddRunCount();
-        SaveVersionToEditor();
+        BuildInfo.AddCount(BuildInfo.tcount.RunCount);
     }
+}
 
-    private void AddRunCount()//Add RunCount everytime the Editor Plays
-    {
-        BuildInfo currentBI = ReadBuildData();
-        int rc = int.Parse(currentBI.RunCount);
-        currentBI.RunCount = (rc + 1).ToString().PadLeft(6, '0');
-        Debug.Log("Run Count Updated!");
-        SaveBuildData(currentBI);
-        Debug.Log($"Version: {currentBI.VersionType}.{currentBI.MainVer}.{currentBI.SubVar}.{currentBI.BuildVar}.{currentBI.RunCount}");
-    }
+public class BuildVerUpdate : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
 
-    private void SaveVersionToEditor()//Save new version to ProjectFolder/ProjectSettings.asset
+    public void OnPreprocessBuild(BuildReport report)
     {
-        BuildInfo currentBI = ReadBuildData();
-        string[] data = ReadVersionData();
-        string vertxt = data.Single(xa => xa.Contains("  bundleVersion: "));
-        int ind = Array.IndexOf(data, vertxt);
-        data[ind] = $"  bundleVersion: {currentBI.VersionType}.{currentBI.MainVer}.{currentBI.SubVar}.{currentBI.BuildVar}.{currentBI.RunCount}";
-        SaveVersionData(data);
+        BuildInfo.AddCount(BuildInfo.tcount.BuildCount);
     }
-
-    private BuildInfo ReadBuildData()//Read the previous version from ProjectFolder/Assets/BuildInfo/BuildInfo.json
-    {
-        if (!File.Exists(BuildPath)) SaveBuildData(new BuildInfo());
-        BuildInfo bi = JsonConvert.DeserializeObject<BuildInfo>(File.ReadAllText(BuildPath));
-        return bi;
-    }
-    private void SaveBuildData(BuildInfo bi)//Overwrite the file at ProjectFolder/Assets/BuildInfo/BuildInfo.json
-    {
-        File.WriteAllText(BuildPath, JsonConvert.SerializeObject(bi));
-        AssetDatabase.Refresh();
-    }
-    private string[] ReadVersionData()//Read the current version from ProjectFolder/ProjectSettings/ProjectSettings.asset
-    {
-        string[] filecontent = File.ReadAllLines(DataPath);
-        return filecontent;
-    }
-    private void SaveVersionData(string[] ary)//Overwrite the file at ProjectFolder/ProjectSettings/ProjectSettings.asset
-    {
-        File.WriteAllLines(DataPath, ary);
-        AssetDatabase.Refresh();
-    }
-#endif
 }
 
 public class BuildInfo
 {
+    public enum tcount
+    {
+        MainVer,
+        SubVer,
+        BuildCount,
+        RunCount
+    }
     public string VersionType = "DEV"; 
-    public int MainVer = 0;
-    public int SubVar = 0;
-    public string BuildVar = "00000";
+    public string MainVer = "0";
+    public string SubVer = "0";
+    public string BuildCount = "00000";
     public string RunCount = "00000";
+
+    static string BuildPath = Application.dataPath.ToString() + "/BuildInfo/BuildInfo.json";
+    static string DataPath = Application.dataPath.ToString().TrimEnd("Assets".ToCharArray()) + "ProjectSettings/ProjectSettings.asset";
+
+#region Update
+
+    public static void AddCount(tcount t)
+    {
+        BuildInfo currentBI = ReadBuildInfo();
+        switch (t)
+        {
+            case tcount.MainVer:
+                int mv = int.Parse(currentBI.MainVer);
+                currentBI.MainVer = (mv + 1).ToString().PadLeft(1, '0');
+                break;
+            case tcount.SubVer:
+                int sv = int.Parse(currentBI.SubVer);
+                currentBI.SubVer = (sv + 1).ToString().PadLeft(1, '0');
+                break;
+            case tcount.BuildCount:
+                int bc = int.Parse(currentBI.BuildCount);
+                currentBI.BuildCount = (bc + 1).ToString().PadLeft(6, '0');
+                break;
+            case tcount.RunCount:
+                int rc = int.Parse(currentBI.RunCount);
+                currentBI.RunCount = (rc + 1).ToString().PadLeft(6, '0');
+                break;
+        }
+        Debug.Log("Build Count Updated!");
+        SaveNewVersion(currentBI);
+        Debug.Log($"Version: {currentBI.VersionType}.{currentBI.MainVer}.{currentBI.SubVer}.{currentBI.BuildCount}.{currentBI.RunCount}");
+    }
+#endregion
+
+#region Read/Write
+    static BuildInfo ReadBuildInfo()//Read the previous version from ProjectFolder/Assets/BuildInfo/BuildInfo.json
+    {
+        if (!File.Exists(BuildPath)) SaveNewVersion(new BuildInfo());
+        BuildInfo bi = JsonConvert.DeserializeObject<BuildInfo>(File.ReadAllText(BuildPath));
+        return bi;
+    }
+
+    static void SaveNewVersion(BuildInfo bi)
+    {
+        //Save BuildInfo.json
+        File.WriteAllText(BuildPath, JsonConvert.SerializeObject(bi));
+
+        //Save ProjectSettings.asset
+        string[] data = ReadProjectSettings();
+        string vertxt = data.Single(xa => xa.Contains("  bundleVersion: "));
+        int ind = Array.IndexOf(data, vertxt);
+        data[ind] = $"  bundleVersion: {bi.VersionType}.{bi.MainVer}.{bi.SubVer}.{bi.BuildCount}.{bi.RunCount}";
+        SaveProjectSettings(data);
+    }
+
+    static string[] ReadProjectSettings()//Read the current version from ProjectFolder/ProjectSettings/ProjectSettings.asset
+    {
+        string[] filecontent = File.ReadAllLines(DataPath);
+        return filecontent;
+    }
+
+    static void SaveProjectSettings(string[] ary)//Overwrite the file at ProjectFolder/ProjectSettings/ProjectSettings.asset
+    {
+        File.WriteAllLines(DataPath, ary);
+    }
+    #endregion
 }
+#endif
