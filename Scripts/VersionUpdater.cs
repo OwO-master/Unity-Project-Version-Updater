76fb987162380f1d41e89a8eed3ed71e8fa1e075
Name <kculwpvalxe@users.noreply.github.com>
2020-07-02 13:23:14 +0800

    add comments

--- Scripts/VersionUpdater.cs
+++ Scripts/VersionUpdater.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
@@ -35,7 +36,7 @@ public class VersionUpdater : MonoBehaviour
         SaveVersionToEditor();
     }
 
-    private void AddRunCount()
+    private void AddRunCount()//Add RunCount everytime the Editor Plays
     {
         BuildInfo currentBI = ReadBuildData();
         int rc = int.Parse(currentBI.RunCount);
@@ -45,7 +46,7 @@ public class VersionUpdater : MonoBehaviour
         Debug.Log($"Version: {currentBI.VersionType}.{currentBI.MainVer}.{currentBI.SubVar}.{currentBI.BuildVar}.{currentBI.RunCount}");
     }
 
-    private void SaveVersionToEditor()
+    private void SaveVersionToEditor()//Save new version to ProjectFolder/ProjectSettings.asset
     {
         BuildInfo currentBI = ReadBuildData();
         string[] data = ReadVersionData();
@@ -55,23 +56,23 @@ public class VersionUpdater : MonoBehaviour
         SaveVersionData(data);
     }
 
-    private BuildInfo ReadBuildData()
+    private BuildInfo ReadBuildData()//Read the previous version from ProjectFolder/Assets/BuildInfo/BuildInfo.json
     {
         if (!File.Exists(BuildPath)) SaveBuildData(new BuildInfo());
         BuildInfo bi = JsonConvert.DeserializeObject<BuildInfo>(File.ReadAllText(BuildPath));
         return bi;
     }
-    private void SaveBuildData(BuildInfo bi)
+    private void SaveBuildData(BuildInfo bi)//Overwrite the file at ProjectFolder/Assets/BuildInfo/BuildInfo.json
     {
         File.WriteAllText(BuildPath, JsonConvert.SerializeObject(bi));
         AssetDatabase.Refresh();
     }
-    private string[] ReadVersionData()
+    private string[] ReadVersionData()//Read the current version from ProjectFolder/ProjectSettings/ProjectSettings.asset
     {
         string[] filecontent = File.ReadAllLines(DataPath);
         return filecontent;
     }
-    private void SaveVersionData(string[] ary)
+    private void SaveVersionData(string[] ary)//Overwrite the file at ProjectFolder/ProjectSettings/ProjectSettings.asset
     {
         File.WriteAllLines(DataPath, ary);
         AssetDatabase.Refresh();
